cba12d340bf3f6220407640b45c47edadf42bb90
Himanshu Goyal <himanshu.goyal@live.com>
2018-09-01 20:21:18 +0530

    Adding Constant Service in the application

--- public/app/app.js
+++ public/app/app.js
@@ -3,20 +3,19 @@
     var app = angular.module('app', []);
 
      // Provider function is also exposed as a module object and its not mandatory to inject in config and then use it.
-    app.provider('books', function () {
+    app.provider('books', function (constants) {
 
         // Every provider must have a property $get
         // $get contains the function that will be executing the service for us.
         this.$get = function (){
-            var appName = 'Book Logger';
-            var appDesc = 'Track which books you read.'
-            
-            var version = '1.0';
+            var appName = constants.APP_TITLE;
+            var version = constants.APP_DESCRIPTION;
 
             if(includeVersionInTitle) {
                 appName += ' ' + version;
             }
-
+            var appDesc = constants.APP_DESCRIPTION;
+            
             // The return code contains the object literal that represents the service
             return {
                 appName: appName,
@@ -31,9 +30,11 @@
 
     });
     // angular automatically appends provider
-    app.config(function(booksProvider){
+    app.config(function(booksProvider, constants){
 
        booksProvider.setIncludeVersionInTitle(false);
+
+       console.log('title from constants service: ' + constants.APP_TITLE);
     });
 
 }());
