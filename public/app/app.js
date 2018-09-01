ce404acd8b7eed129c522201784fa1bca5a16ff2
Himanshu Goyal <himanshu.goyal@live.com>
2018-09-01 18:51:53 +0530

    Using Provide by making it configurable

--- public/app/app.js
+++ public/app/app.js
@@ -2,25 +2,38 @@
 
     var app = angular.module('app', []);
 
-    app.config(function($provide){
+     // Provider function is also exposed as a module object and its not mandatory to inject in config and then use it.
+    app.provider('books', function () {
 
-        // Benefit of provider is that it is configurable during the module configuration phase
-        $provide.provider('books', function () {
+        // Every provider must have a property $get
+        // $get contains the function that will be executing the service for us.
+        this.$get = function (){
+            var appName = 'Book Logger';
+            var appDesc = 'Track which books you read.'
+            
+            var version = '1.0';
 
-            // Every provider must have a property $get
-            // $get contains the function that will be executing the service for us.
-            this.$get = function (){
-                var appName = 'Book Logger';
-                var appDesc = 'Track which books you read.'
-                
+            if(includeVersionInTitle) {
+                appName += ' ' + version;
+            }
 
-                // The return code contains the object literal that represents the service
-                return {
-                    appName: appName,
-                    appDesc: appDesc
-                };
+            // The return code contains the object literal that represents the service
+            return {
+                appName: appName,
+                appDesc: appDesc
             };
-        });
+        };
+
+        var includeVersionInTitle = false;
+        this.setIncludeVersionInTitle = function (value) {
+            includeVersionInTitle = value;
+        };
+
+    });
+    // angular automatically appends provider
+    app.config(function(booksProvider){
+
+       booksProvider.setIncludeVersionInTitle(false);
     });
 
 }());
