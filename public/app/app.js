7cbe50119a5916530cb7845dd5052aa9b6a5c5b2
Himanshu Goyal <himanshu.goyal@live.com>
2018-09-02 09:14:14 +0530

    Routing - $RouteChange and handling events

--- public/app/app.js
+++ public/app/app.js
@@ -65,6 +65,25 @@
         .otherwise('/');
     }]);
 
-    
+    // angular has a function name run, which act as an initialization module
+    app.run(['$rootScope', function($rootScope){
+        $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+
+            console.log('successfully changed routes');
+
+        });
+
+        $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+
+            console.log('error changing routes');
+
+            console.log(event);
+            console.log(current);
+            console.log(previous);
+            console.log(rejection);
+
+        });
+
+    }])
 
 }());
